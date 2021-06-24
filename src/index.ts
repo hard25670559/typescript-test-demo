59f01853172403782980502f559228dfe68677b7
hardanonymous <hard25670559@gmail.com>
2021-06-25 00:10:28 +0800

    add outside lib reference

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,45 @@
-import Vue from 'vue';
-import {clone} from 'lodash-es';
+/// <reference path="/Users/hardanonymous/Project/typescript/test01/node_modules/axios/index.d.ts" />
+/// <reference path="/Users/hardanonymous/Project/typescript/test01/node_modules/vue/types/index.d.ts" />
+/// <reference path="/Users/hardanonymous/Project/typescript/test01/node_modules/@types/lodash-es/clone.d.ts" />
+/// <reference path="/Users/hardanonymous/Project/typescript/test01/node_modules/@types/jquery/index.d.ts" />
 
-console.log(clone({}));
+console.log($('#app'));
 
 let target = document.querySelector('#app');
 console.log(target);
 
-const vue = new Vue({
-    el: target!,
-    data: () => ({
-        message: 'hello world',
-    }),
-    created() {
-        console.log(this, vue);
-        console.log(this.message);
+type optionData = {
+    message: string,
+    list: number[],
+}
+
+let data: optionData = {
+    message: 'hello world',
+    list: [],
+}
+
+new Vue({
+    el: '#app',
+    data,
+    async created() {
+        let google = await axios.get('https://google.com.tw');
+        let a = new Promise<number[]>((resolve, reject) => {
+            setTimeout(() => {
+                resolve([1, 2, 3]);
+            }, 2000);
+        });
+
+        data.list = await a;
     },
     mounted() {
-        console.log(this, vue);
+        console.log(data.message);
     },
+    methods: {
+        changeMessage() {
+            data.message = 'abc';
+        },
+        addListEle() {
+            data.list.push(1);
+        }
+    }
 });
-
-console.log(vue);
