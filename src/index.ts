1eb8bf32d1376fbb0efe7ccedda05cc1fe03a609
Hardanonymous <hard@microsys.com.tw>
2021-06-25 01:31:19 +0800

    backup

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,33 @@
-/// <reference path="/Users/hardanonymous/Project/typescript/test01/node_modules/axios/index.d.ts" />
-/// <reference path="/Users/hardanonymous/Project/typescript/test01/node_modules/vue/types/index.d.ts" />
-/// <reference path="/Users/hardanonymous/Project/typescript/test01/node_modules/@types/lodash-es/clone.d.ts" />
-/// <reference path="/Users/hardanonymous/Project/typescript/test01/node_modules/@types/jquery/index.d.ts" />
+/// <reference path="../types/axios.d.ts" /> 
+/// <reference path="../node_modules/vue/types/index.d.ts" />
+/// <reference path="../node_modules/@types/lodash/index.d.ts" />
+/// <reference path="../node_modules/@types/jquery/index.d.ts" />
 
-console.log($('#app'));
 
+// JQUERY
+$('#app').append('<h1> This is append from jquery.</h1>');
+
+// LODASH
+let cloneDataTmp = {
+    a: 1,
+};
+let cloneData = _.clone(cloneDataTmp);
+console.log(cloneData);
+
+// VUE
 let target = document.querySelector('#app');
 console.log(target);
 
-type optionData = {
+type VueData = {
     message: string,
     list: number[],
 }
 
-let data: optionData = {
+type ResponseData = {
+    data: number[]
+}
+
+let data: VueData = {
     message: 'hello world',
     list: [],
 }
@@ -22,14 +36,14 @@ new Vue({
     el: '#app',
     data,
     async created() {
-        let google = await axios.get('https://google.com.tw');
-        let a = new Promise<number[]>((resolve, reject) => {
-            setTimeout(() => {
-                resolve([1, 2, 3]);
-            }, 2000);
-        });
+        // AXIOS無法直接使用官方提供的定義檔，所以自己拷貝了一份
+        // 目前只有發現將export的部分刪除就可以了，還不清楚為什麼
+        let response = await axios.get<ResponseData>('/resource/test.json');
+        console.log(response.data.data);
 
-        data.list = await a;
+        // 該部分的data原先的寫法會是直接使用this，但typescript的關係無法直接這樣子使用
+        // 要從外部將資料匯入才可以做使用！
+        data.list = response.data.data;
     },
     mounted() {
         console.log(data.message);
